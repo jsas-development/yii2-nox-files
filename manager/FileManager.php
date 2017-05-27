3f8ede69458b73fd2d9094d9bbafe7997a9194f1
Jonatas Sas <atendimento@jsas.com.br>
2017-05-26 22:50:23 -0300

    0.0.2

---
+++ manager/FileManager.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace nox\files\manager;
+
+    use yii\base\Component;
+
+    /**
+     * Class FileManager
+     *
+     * @package nox\files\manager
+     */
+    class FileManager extends Component
+    {
+
+    }
